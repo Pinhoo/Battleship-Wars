f5141f9871855624d2b12341936a65a1749787de
André Pinho <160323023@estudantes.ips.pt>
2018-03-26 17:06:09 +0100

    Coordenadas Model

---
+++ BattleshipPRJ/Models/Coordenadas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleshipPRJ.Models
+{
+    public class Coordenadas
+    {
+        public int SelectedCoordX { get; set; }
+
+        public int SelectedCoordY { get; set; }
+
+        public Coordenadas()
+        {
+
+
+        }
+
+    }
+}
