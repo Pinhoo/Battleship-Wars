84d6ba69f32fb482a5c59ad7df17d437298f4f35
André Pinho <160323023@estudantes.ips.pt>
2018-04-01 19:26:18 +0100

    view "Regras" acrescentada, fix do title de todas as views, css das tabelas

--- BattleshipPRJ/Controllers/HomeController.cs
+++ BattleshipPRJ/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace BattleshipPRJ.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Regras()
         {
             return View();
         }
