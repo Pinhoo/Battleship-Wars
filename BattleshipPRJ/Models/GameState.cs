0a9a4b0a95953c3002b9ccba6cfc77edd6fd83d4
André Filipe Gomes Pinho <160323023@estudantes.ips.pt>
2018-05-12 19:45:15 +0100

    GameState class

---
+++ BattleshipPRJ/Models/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace BattleshipPRJ.Models
+{
+    public class GameState
+    {
+
+        public int GameID { get; set; }
+
+        public int RoundNumber { get; set; }
+
+        public int FiredX { get; set; }
+
+        public int FiredY { get; set; }
+
+        public enum Result { NoResult, SuccessHit, SuccessMiss, SuccessSink, SuccessRepeat, SuccessVictory, InvalidShot, GameHasEnded }
+
+        public int DamagedShipSize { get; set; }
+
+    }
+
+
+}
+
