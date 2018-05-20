61dbe716539bfa8f753e5492aefe0f0cf4c9cd21
André Filipe Gomes Pinho <160323023@estudantes.ips.pt>
2018-05-20 20:45:46 +0100

    Desistir como PlayerAction.Quit

--- BattleshipPRJ/Controllers/GameController.cs
+++ BattleshipPRJ/Controllers/GameController.cs
@@ -226,8 +226,25 @@ namespace BattleshipPRJ.Controllers
             }
             else if(submitButton== "Desistir")
             {
+
+
+                HttpClient client = MyHttpClient.Client;
+                string path = "api/Play";
+                PlayRequest pr = new PlayRequest(id, opcaoX, opcaoY, PlayerAction.Quit);
+                string json = JsonConvert.SerializeObject(pr);
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, path);
+                request.Content = new StringContent(json, System.Text.Encoding.UTF8,
+                "application/json");
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode) { return Redirect("/"); }
+                string json_r = await response.Content.ReadAsStringAsync();
+                GameState gs = JsonConvert.DeserializeObject<GameState>(json_r);
+
                 jogue.FimdoJogo = "Derrota!";
                 jogue.Gameover = true;
+
                 return View(jogue);
 
             }
@@ -331,7 +348,7 @@ namespace BattleshipPRJ.Controllers
                     jogue.Disparou(gs.DamagedShipSize, true, true);
                     jogue.FimdoJogo = "Ganhaste!";
 
-                    return View("GameOver", jogue);
+                    return View(jogue);
                 }
                 else if (gs.Result == Resultado.InvalidShot)
                 {
@@ -341,7 +358,7 @@ namespace BattleshipPRJ.Controllers
                 {
                     jogue.FimdoJogo = "Perdeste!";
 
-                    return View("GameOver", jogue);
+                    return View(jogue);
                 }
                 else if (gs.Result == Resultado.NoResult)
                 {
